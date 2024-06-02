baea65f7e300babfcc0f91c410ee11a96d6cb49b
fhassidiqi <falahhasbiassidiqi@upi.edu>
2024-06-02 11:50:10 +0700

    Initial Commit

---
+++ NewsPBI/NewsPBIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsPBIApp.swift
+//  NewsPBI
+//
+//  Created by Falah Hasbi Assidiqi on 02/06/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsPBIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
